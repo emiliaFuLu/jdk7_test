6bc13c9669a1831852f89c97a196bb467e49253e
fulu <1039897786@qq.com>
2021-06-11 17:39:59 +0800

    feat:add jdk7 source

---
+++ src/com/fl/test.java
@@ -0,0 +1,18 @@
+package com.fl;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: fl
+ * @date: 2021-06-11 2:04 下午
+ * @description:
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>(7);
+        map.put("1","1");
+    }
+}
